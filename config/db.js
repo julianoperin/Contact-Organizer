de403015718f92eeff515b3cf9e589d86fd9edb1
‘Juliano <julianoperins@gmail.com>
2020-12-14 14:45:02 -0500

    Created validation for the user registration

---
+++ config/db.js
@@ -0,0 +1,40 @@
+const { Mongoose } = require("mongoose");
+
+const mongoose = require("mongoose");
+
+const config = require("config"); //! Using the dependency that we installed
+
+const db = config.get("mongoURI"); //! Get the default JSON from default
+
+//! Connection With ASYNC AWAIT
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected :)");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+  //! Connection With Promises
+  // const connectDB = () => {
+  //   mongoose
+  //     .connect(db, {
+  //       useNewUrlParser: true,
+  //       useCreateIndex: true,
+  //       useFindAndModify: false,
+  //     })
+  //     .then(() => console.log("mongoDB Connected!"))
+  //     .catch((err) => {
+  //       console.log(err);
+  //       process.exit(1);
+  //     });
+  // };
+};
+module.exports = connectDB;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,34 @@
 const express = require("express");
-
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-//! @route POST api/users
+//! Import the User Schema
+
+const User = require("../models/User");
+
+//! @route POST "api/users"
 //! @desc Register
 //! @access Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  //! Express validation for NAME, EMAIL and PASSWORD
+  [
+    check("name", "Please, name is required").not().isEmpty(),
+    check("email", "Please, use a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  //! REQ - RES
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("passed");
+  }
+);
 
 module.exports = router;
