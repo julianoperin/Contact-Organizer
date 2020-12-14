878e9334591ce811a4883ce4e323421ecd0bbd0d
‘Juliano <julianoperins@gmail.com>
2020-12-14 15:35:57 -0500

    Worked on authentication with jwt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
 //! Import the User Schema
@@ -52,7 +54,23 @@ router.post(
 
       await user.save();
 
-      res.send("User saved!");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
