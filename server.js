bacbfea1ab4370c33fb33134ab928ba7658b69e6
‘Juliano <julianoperins@gmail.com>
2021-02-05 16:53:01 -0500

    fixed the bug

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+app.set("port", PORT);
 
 //! Connect DATABASE
 connectDB();
@@ -17,15 +21,13 @@ app.use("/api/contacts", require("./routes/contacts"));
 
 //! Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
+  //! Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
