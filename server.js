3bfe1d6d4f9472eed4df2dc4f8c74582977ae657
‘Juliano <julianoperins@gmail.com>
2021-02-05 17:18:15 -0500

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
-
-app.set("port", PORT);
 
 //! Connect DATABASE
 connectDB();
@@ -28,6 +24,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
